8eb33afdf5f45c5fee7e76273b4f5999818eded0
krafen <33376586+krafen@users.noreply.github.com>
2026-02-27 12:50:38 +0100

    Update streamlit_app_2.py

--- streamlit_app_2.py
+++ streamlit_app_2.py
@@ -84,7 +84,7 @@ def set_background(image_path):
         unsafe_allow_html=True
     )
 
-set_background()
+set_background("assets/background.jpg")
 
 FIRST_FILE = "Slanger_hylser.xlsx"
 SECOND_FILE = "kuplinger_316.xlsx"
