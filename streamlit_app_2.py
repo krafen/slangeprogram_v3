2a474dd0f11d27d115c5a27cac9fac9e62995370
krafen <33376586+krafen@users.noreply.github.com>
2026-02-27 15:05:22 +0100

    Update streamlit_app_2.py

--- streamlit_app_2.py
+++ streamlit_app_2.py
@@ -131,7 +131,7 @@ def set_background(image_path):
         }}
     
         /* === CHECKBOX TEXT (white) === */
-        .stCheckbox > p{{
+        .stCheckbox > p {{
             color: white !important;
         }}
     
